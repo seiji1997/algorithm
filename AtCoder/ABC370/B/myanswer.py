e51d3c92e971fbbb312d6112cf6dc5b1050de862
seijitanimoto <seijitanimoto1997@gmail.com>
2024-09-08 13:33:20 +0900

    ABC370 A B C

--- AtCoder/ABC370/B/myanswer.py
+++ AtCoder/ABC370/B/myanswer.py
@@ -1,1 +1,19 @@
-# This is the myanswer.py for problem B in ABC370
+def find_element(N, A):
+    result = 1
+
+    for i in range(2, N + 1):
+        if i >= result:
+            result = A[i - 1][result - 1]
+        else:
+            result = A[result - 1][i - 1]
+
+    return result
+
+
+N = int(input())
+A = []
+
+for i in range(N):
+    A.append(list(map(int, input().split())))
+
+print(find_element(N, A))
--- AtCoder/ABC370/C/myanswer.py
+++ AtCoder/ABC370/C/myanswer.py
@@ -1,1 +1,19 @@
-# This is the myanswer.py for problem C in ABC370
+def solve(S, T):
+    n = len(S)
+    X = []
+    S = list(S)
+
+    for i in range(n):
+        if S[i] != T[i]:
+            S[i] = T[i]
+            X.append("".join(S))
+
+    print(len(X))
+    for x in X:
+        print(x)
+
+
+S = input().strip()
+T = input().strip()
+
+solve(S, T)
