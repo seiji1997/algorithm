f16d7749a30a4f21aa42a3b95c3aff8d60bbc99e
seijitanimoto <seijitanimoto1997@gmail.com>
2024-09-12 17:51:03 +0900

    ABC361 A C

--- AtCoder/ABC361/A/myanswer.py
+++ AtCoder/ABC361/A/myanswer.py
@@ -1,1 +1,11 @@
 # This is the myanswer.py for problem A in ABC361
+
+N, K, X = map(int, input().split())
+A = list(map(int, input().split()))
+
+# B = A[:K] + [X] + A[K:]
+
+B = A.copy()
+B.insert(K, X)
+
+print(" ".join(map(str, B)))
--- AtCoder/ABC361/B/myanswer.py
+++ AtCoder/ABC361/B/myanswer.py
@@ -1,1 +1,44 @@
 # This is the myanswer.py for problem B in ABC361
+
+def min_max_difference(N, K, A):
+    A.sort()
+    min_diff = float('inf')
+    
+    for i in range(N - K + 1):
+        current_diff = A[i + (N - K - 1)] - A[i]
+        if current_diff < min_diff:
+            min_diff = current_diff
+    
+    return min_diff
+
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+print(min_max_difference(N, K, A))
+
+# -----------------------------------
+
+import sys
+input = sys.stdin.read
+data = input().split()
+
+N = int(data[0])
+K = int(data[1])
+A = list(map(int, data[2:]))
+
+def find_min_diff(N, K, A):
+    A.sort()
+    min_diff = float('inf')
+    
+    for i in range(K + 1):
+        diff = A[i + (N - K) - 1] - A[i]
+        if diff < min_diff:
+            min_diff = diff
+    
+    return min_diff
+
+result = find_min_diff(N, K, A)
+print(result)%                      
+
+
+
