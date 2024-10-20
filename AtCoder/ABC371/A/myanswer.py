eb0f7eae13e23175f86cd35cb38e063b8b69a4eb
seijitanimoto <seijitanimoto1997@gmail.com>
2024-10-20 13:34:27 +0900

    ABC371 A

--- AtCoder/ABC371/A/myanswer.py
+++ AtCoder/ABC371/A/myanswer.py
@@ -1,1 +1,20 @@
 # This is the myanswer.py for problem A in ABC371
+
+SAB, SAC, SBC = input().split()
+
+if SAB == "<" and SAC == "<" and SBC == "<":
+    print("B")
+elif SAB == "<" and SAC == "<" and SBC == ">":
+    print("C")
+elif SAB == "<" and SAC == ">" and SBC == "<":
+    print("C")
+elif SAB == "<" and SAC == ">" and SBC == ">":
+    print("A")
+elif SAB == ">" and SAC == "<" and SBC == "<":
+    print("A")
+elif SAB == ">" and SAC == "<" and SBC == ">":
+    print("B")
+elif SAB == ">" and SAC == ">" and SBC == "<":
+    print("A")
+elif SAB == ">" and SAC == ">" and SBC == ">":
+    print("B")
