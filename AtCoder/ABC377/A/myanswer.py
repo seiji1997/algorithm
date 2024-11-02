19d8f575e190942a9c469cf7bfff840f8efad887
seijitanimoto <seijitanimoto1997@gmail.com>
2024-11-02 18:01:27 +0900

    ABC377 A B

--- AtCoder/ABC377/A/myanswer.py
+++ AtCoder/ABC377/A/myanswer.py
@@ -1,1 +1,15 @@
 # This is the myanswer.py for problem A in ABC377
+
+import sys
+from itertools import permutations
+
+
+def check_string(S):
+    if "ABC" in map("".join, permutations(S)):
+        return "Yes"
+    else:
+        return "No"
+
+
+S = input().strip()
+print(check_string(S))
--- AtCoder/ABC377/B/myanswer.py
+++ AtCoder/ABC377/B/myanswer.py
@@ -1,1 +1,21 @@
 # This is the myanswer.py for problem B in ABC377
+def count_available_cells(grid):
+    rows_with_hash = set()
+    cols_with_hash = set()
+
+    for i in range(8):
+        for j in range(8):
+            if grid[i][j] == "#":
+                rows_with_hash.add(i)
+                cols_with_hash.add(j)
+
+    row_blocked = len(rows_with_hash) * 8
+    col_blocked = len(cols_with_hash) * (8 - len(rows_with_hash))
+    total_blocked = row_blocked + col_blocked
+    available_cells = 64 - total_blocked
+
+    return available_cells
+
+
+grid = list(map(str.strip, [input() for _ in range(8)]))
+print(count_available_cells(grid))
